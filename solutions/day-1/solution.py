ed5aea086303ecde44f203143c881d746d88ed19
Milk Web <0xwebmilk@gmail.com>
2024-12-01 17:45:12 +0300

    1 Day solution added

--- solutions/day-1/solution.py
+++ solutions/day-1/solution.py
@@ -1,4 +1,4 @@
-def main(file_path: str) -> int:
+def part1(file_path: str) -> int:
     left = []
     right = []
 
@@ -14,6 +14,21 @@ def main(file_path: str) -> int:
 
     return sum([abs(right[i] - left[i]) for i in range(len(left))])
 
+def part2(file_path: str) -> int:
+    left = []
+    right = []
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            numbers = line.split()
+            
+            left.append(int(numbers[0]))
+            right.append(int(numbers[1]))
+
+
+    return sum([left[i] * right.count(left[i]) for i in range(len(left))])
+
 
 if __name__ == "__main__":
-    main('./input.txt')
+    part1('./input.txt')
+    part2('./input.txt')
